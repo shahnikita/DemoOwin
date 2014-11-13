96db2989f690124b41a62011e4a8040c6adfa846
shahnikita <shahnikita.1991@yahoo.com>
2014-11-13 15:18:15 +0530

    Added my project

---
+++ DemoOwin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoOwin.Startup))]
+namespace DemoOwin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
